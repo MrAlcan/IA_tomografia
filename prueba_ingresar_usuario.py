1b5c71e40554b5e782cd03a6d841369cccc75a8f
JuanCarlosApazaGutierrez <128553021+JuanCarlosApazaGutierrez@users.noreply.github.com>
2024-09-06 11:57:40 -0400

    cambio

---
+++ prueba_ingresar_usuario.py
@@ -0,0 +1,6 @@
+from app.servicios.serviciosUsuario import ServiciosUsuario
+ 
+ 
+ki = ServiciosUsuario.crear(ap_materno="yujra", ap_paterno="chambi", cargo="hh",carnet="1286",contrasena="ard",id_rol="1",nombre="ca",nombre_per="sdf")
+ 
+print(ki)
